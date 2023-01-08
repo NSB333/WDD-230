490a737881ef927db9edf6c336fdc02e7000ea90
Cecelia <CeceliaPendell@gmail.com>
2023-01-07 17:07:36 -0700

    Ahhh

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ let year = date.getFullYear();
 
 let copyright = `\u00a9 $(year) -Cece Pendell-`;
 document.getElementById("year").textcontent = copyright;
+alert(year);
 
 let currentdate = document.lastModified;
 
